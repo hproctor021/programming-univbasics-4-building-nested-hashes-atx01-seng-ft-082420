c85f4cb445c711ff70d4bb80e5fc348dada2bd54
Haley Proctor <hproctor021@gmail.com>
2020-08-01 18:20:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ def monopoly_with_second_tier
   new_hash = {
     :railroads => {}
   }
-new_hash[:railroads] = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
+new_hash = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
 end
 
 def monopoly_with_third_tier
